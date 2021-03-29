88a4cb5293d354af4ea7884183fc3dc28504134b
Christian Gessner <christian@gessner.cc>
2021-03-29 07:54:28 +0200

    Removed logo.png

--- vapiTraceFiddlerExtension/Properties/Resources.Designer.cs
+++ vapiTraceFiddlerExtension/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace vapiTraceFiddlerExtension.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap logo {
-            get {
-                object obj = ResourceManager.GetObject("logo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
